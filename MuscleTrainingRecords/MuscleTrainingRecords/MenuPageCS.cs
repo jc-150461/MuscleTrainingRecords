f0ce922ef003c221e96a301451486a57dc031bda
燎 石橋 <150461@jc-21.jp>
2017-12-15 17:59:10 +0900

    fix

---
+++ MuscleTrainingRecords/MuscleTrainingRecords/MenuPageCS.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Xamarin.Forms;
+
+namespace MuscleTrainingRecords
+{
+    public class MenuPageCS : ContentPage
+    {
+        public MenuPageCS()
+        {
+            Title = "メニュー一覧";
+            Content = new StackLayout
+            {
+                Children = {
+                    new Label {
+                        Text = "Welcome to MenuPage!",
+                        HorizontalOptions = LayoutOptions.Center,
+                        VerticalOptions = LayoutOptions.CenterAndExpand
+                    }
+                },
+            };
+        }
+    }
+}
