a0f671981a65fbed1e1b2cf5f788f9c9cbf3c424
燎 石橋 <150461@jc-21.jp>
2017-12-17 03:53:32 +0900

    fix

--- MuscleTrainingRecords/MuscleTrainingRecords/LineChart.cs
+++ MuscleTrainingRecords/MuscleTrainingRecords/LineChart.cs
@@ -23,18 +23,18 @@ namespace MuscleTrainingRecords
             var X_line = new LineSeries() { Title="体重"};
            
             X_line.Color = OxyColors.Red;
-            X_line.Points.Add(new DataPoint(0, 0));
-            X_line.Points.Add(new DataPoint(1, 4));
-            X_line.Points.Add(new DataPoint(2, 1));
+            X_line.Points.Add(new DataPoint(0, 1));
+            X_line.Points.Add(new DataPoint(1, 2));
+            X_line.Points.Add(new DataPoint(2, 3));
             X_line.Points.Add(new DataPoint(3, 3));
             Model.Series.Add(X_line);
 
             var Y_line = new LineSeries() { Title="体脂肪"};
             Y_line.Color = OxyColors.Blue;
-            Y_line.Points.Add(new DataPoint(0, 0));
+            Y_line.Points.Add(new DataPoint(0, 2));
             Y_line.Points.Add(new DataPoint(1, 2));
-            Y_line.Points.Add(new DataPoint(2, 0));
-            Y_line.Points.Add(new DataPoint(3, 6));
+            Y_line.Points.Add(new DataPoint(2, 2));
+            Y_line.Points.Add(new DataPoint(3, 2));
             Model.Series.Add(Y_line);
         }
     }
