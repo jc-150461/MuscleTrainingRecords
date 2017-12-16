a797fff28a4c65539026f0293994ed0fb93e1178
燎 石橋 <150461@jc-21.jp>
2017-12-16 20:27:03 +0900

    fix

--- MuscleTrainingRecords/MuscleTrainingRecords/LineChart.cs
+++ MuscleTrainingRecords/MuscleTrainingRecords/LineChart.cs
@@ -1,19 +1,25 @@
-﻿using System.Collections.Generic;
-
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 using OxyPlot;
+using OxyPlot.Series;
 
 
 namespace MuscleTrainingRecords
 {
     class LineChart
     {
-        //public PlotModel Model { get; private set; }
+        public PlotModel Model { get; private set; }
 
         public LineChart()
         {
-            /*this.Model = new PlotModel { Title = "" };
+            this.Model = new PlotModel { Title = "" };
 
-            var X_line = new LineSeries();
+            
+            var X_line = new LineSeries() { Title="体重"};
+           
             X_line.Color = OxyColors.Red;
             X_line.Points.Add(new DataPoint(0, 0));
             X_line.Points.Add(new DataPoint(1, 4));
@@ -21,38 +27,13 @@ namespace MuscleTrainingRecords
             X_line.Points.Add(new DataPoint(3, 3));
             Model.Series.Add(X_line);
 
-            var Y_line = new LineSeries();
+            var Y_line = new LineSeries() { Title="体脂肪"};
             Y_line.Color = OxyColors.Blue;
             Y_line.Points.Add(new DataPoint(0, 0));
             Y_line.Points.Add(new DataPoint(1, 2));
             Y_line.Points.Add(new DataPoint(2, 0));
             Y_line.Points.Add(new DataPoint(3, 6));
             Model.Series.Add(Y_line);
-            */
-           /************************************ここからついか**************************************/
-            DataList1 = new List<DataPoint>
-            {
-                {new DataPoint(0, 0)},
-                {new DataPoint(2, 4)},
-                {new DataPoint(5, 8)},
-                {new DataPoint(8, 3)},
-                {new DataPoint(12, 5)},
-            };
-
-
-            DataList2 = new List<DataPoint>
-            {
-                {new DataPoint(0, 0)},
-                {new DataPoint(2, 4)},
-                {new DataPoint(5, 8)},
-                {new DataPoint(8, 3)},
-                {new DataPoint(12, 5)},
-            };
         }
-
-         public List<DataPoint> DataList1 { get; }
-         public List<DataPoint> DataList2 { get; }
-        /************************************ここからまで**************************************/
     }
 }
-
