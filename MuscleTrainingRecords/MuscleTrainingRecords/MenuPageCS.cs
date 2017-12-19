087b7681dcf872c9bab47bb36303255670dbf03d
燎 石橋 <150461@jc-21.jp>
2017-12-19 12:26:18 +0900

    fix

--- MuscleTrainingRecords/MuscleTrainingRecords/MenuPageCS.cs
+++ MuscleTrainingRecords/MuscleTrainingRecords/MenuPageCS.cs
@@ -2,12 +2,15 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading.Tasks;
 
 using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
 
 namespace MuscleTrainingRecords
 {
-    public class MenuPageCS : ContentPage
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public  partial class MenuPageCS : ContentPage
     {
         public MenuPageCS()
         {
