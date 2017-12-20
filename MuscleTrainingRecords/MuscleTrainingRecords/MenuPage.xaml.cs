c3a492cb7e3a2bcc49933baed1556c0b04370636
燎 石橋 <150461@jc-21.jp>
2017-12-20 14:00:46 +0900

    fix

--- MuscleTrainingRecords/MuscleTrainingRecords/MenuPage.xaml.cs
+++ MuscleTrainingRecords/MuscleTrainingRecords/MenuPage.xaml.cs
@@ -106,10 +106,10 @@ namespace MuscleTrainingRecords
             listView.ItemsSource = array1;
         }
 
-        private void listView_ItemTapped(object sender, ItemTappedEventArgs e)
+       /* private void listView_ItemTapped(object sender, ItemTappedEventArgs e)
         {
             Navigation.PopAsync(new MenudetailPage());
-        }
+        }*/
     }
 }
 
